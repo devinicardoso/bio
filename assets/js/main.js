9bee344379c3b79bbcd9817fd5fe6a49d40120d0
Vinicius Cardoso <75495504+vinicxxl@users.noreply.github.com>
2021-11-08 20:48:05 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,3 @@
 function portifolio() {
     alert('Estamos trabalhando nesta página! Volte em outro momento 🚧🚧')
 }
-
-var i = setInterval(function () {
-    
-    clearInterval(i);
-  
-    // O código desejado é apenas isto:
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("conteudo").style.display = "inline";
-
-}, 3000);
-
