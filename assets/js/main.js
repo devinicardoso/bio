5edaf5b82e892fd444f0bdf9d94329484b1b8d1c
Vini Cardoso <vinic8670@gmail.com>
2022-03-16 00:10:08 -0300

    Animacoes nos botoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
-// function portifolio() {
-//     alert('Estamos trabalhando nesta página! Volte em outro momento 🚧🚧')
-// }
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.card1', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 1500
+});
+
+sr.reveal('.card2', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 1500
+});
+
+sr.reveal('.card3', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 2500
+});
+
+sr.reveal('.card4', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 2500
+});
+
+sr.reveal('.card5', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 3500
+});
