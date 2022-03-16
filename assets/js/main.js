88cc452a2f64a6c579f7a44cd170b9b9286e0915
Vini Cardoso <vinic8670@gmail.com>
2022-03-16 00:12:56 -0300

    Configurando animacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ sr.reveal('.card1', {
 });
 
 sr.reveal('.card2', {
-    rotate: { x: 0, y: 80, z: 0 },
+    rotate: { x: 0, y: -80, z: 0 },
     duration: 1500
 });
 
@@ -16,11 +16,11 @@ sr.reveal('.card3', {
 });
 
 sr.reveal('.card4', {
-    rotate: { x: 0, y: 80, z: 0 },
+    rotate: { x: 0, y: -80, z: 0 },
     duration: 2500
 });
 
 sr.reveal('.card5', {
-    rotate: { x: 0, y: 80, z: 0 },
+    rotate: { x: 0, y: 90, z: 0 },
     duration: 3500
 });
